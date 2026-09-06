be8737bde8d1fb1a65160ab9abef2f87ca2140b0
Bookmarks Dev <bookmarks@local>
2026-09-06 16:00:09 +0000

    chore(quality): phpstan level 8 config + php-cs-fixer @Symfony:risky rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,17 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
-    ->notPath([
-        'config/bundles.php',
-        'config/reference.php',
-    ])
-;
+    ->in([__DIR__.'/src', __DIR__.'/tests'])
+    ->exclude('var');
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
+        '@PSR12' => true,
         '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'declare_strict_types' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'phpdoc_align' => false,
+        'yoda_style' => false,
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
